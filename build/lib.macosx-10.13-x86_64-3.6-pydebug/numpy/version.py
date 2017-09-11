90f62961f17052c2bcdbb621eaeb05f78c36db2c
ralic <raliclo@gmail.com>
2017-09-11 13:20:09 -0700

    Add dist/build, using mpicc-clang5

---
+++ build/lib.macosx-10.13-x86_64-3.6-pydebug/numpy/version.py
@@ -0,0 +1,12 @@
+
+# THIS FILE IS GENERATED FROM NUMPY SETUP.PY
+#
+# To compare versions robustly, use `numpy.lib.NumpyVersion`
+short_version = '1.14.0'
+version = '1.14.0'
+full_version = '1.14.0.dev0+68a58e0'
+git_revision = '68a58e03a1661b8d95877771f6e7fa87ddf7c183'
+release = False
+
+if not release:
+    version = full_version
